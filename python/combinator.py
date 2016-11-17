20ff5b22805a6f4a20cb795c022da2ba5bafce13
matsub <matsub.rk@gmail.com>
2016-11-18 02:49:56 +0900

    dark

--- python/combinator.py
+++ python/combinator.py
@@ -1,8 +1,44 @@
-# coding: utf-8
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
 Z = lambda f: (lambda x: f(lambda y: x(x)(y)))(lambda x: f(lambda y: x(x)(y)))
-g = lambda f: (lambda n: (lambda x=0: (lambda y=1: f(n-1)(y)(x+y) if n else x)))
-gcd = lambda f: (lambda a: (lambda b: f(b)(a%b) if b else a))
+sum_from_one = Z(lambda f: lambda n: f(n-1)+n if n else 0)
+gcd = Z(lambda f: lambda a: lambda b: f(b)(a%b) if b else a)
 
-print(Z(g)(10)()())
-print(Z(gcd)(12)(8))
+print(sum_from_one(10))
+print(gcd(12)(8))
+
+
+true = lambda x: lambda y: x
+false = lambda x: lambda y: y
+
+Lif = lambda boolean: lambda M: lambda N: boolean(M)(N)
+print(Lif(true)(True)(False))
+print(Lif(false)(True)(False))
+
+gcd_true = lambda x: lambda y: x(None)
+gcd_bool = lambda x: gcd_true if x else false
+gcd = Z(lambda f: lambda a: lambda b: Lif(gcd_bool(b))(lambda g: f(b)(a%b))(a))
+print(gcd(12)(8))
+
+
+GCD = (lambda f:
+    (lambda x:
+        f(lambda y: x(x)(y))
+    )(lambda x:
+        f(lambda y: x(x)(y))
+    )
+)(lambda f: lambda a: lambda b:
+    (lambda L: lambda M: lambda N:
+        L(M)(N)
+    )(
+        (lambda L:
+            (lambda x: lambda y: x(lambda x: x))
+            if L else (lambda x: lambda y: y)
+        )(
+            b)
+    )(lambda g:
+        f(b)(a%b)
+    )(a)
+)
+print(GCD(12)(8))
