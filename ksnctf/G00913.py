13c916c5f94bac2038f64d61b19eea7fc4272f66
matsub <matsub.rk@gmail.com>
2017-05-11 12:57:16 +0900

    f

--- ksnctf/G00913.py
+++ ksnctf/G00913.py
@@ -2,27 +2,32 @@
 # coding: utf-8
 
 
-def pi_prime(f):
-    def sieve(n):
-        s = list(range(n+1))
-        for i in s[2:]:
-            if s[i] == 0:
-                continue
-            for j in range(i*2, n+1, i):
-                s[j] = 0
-        return filter(lambda x: x, s[2:])
+def sieve(n):
+    s = list(range(n + 1))
+    for i in s[2:]:
+        if s[i] == 0:
+            continue
+        for j in range(i * 2, n + 1, i):
+            s[j] = 0
+    return filter(lambda x: x, s[2:])
 
+
+def is_prime(n, primes):
+    for prime in primes:
+        if (n % prime) == 0:
+            return False
+    return True
+
+
+def d_prime(digset):
     digit = 10
-    primes = list(sieve(100000))
-    def is_prime(n):
-        for prime in primes:
-            if (n%prime)==0:
-                return False
-        return True
+    primes = list(sieve(10**(digit // 2)))
+    for head in range(len(digset) - digit):
+        target = int(digset[head:head + digit])
+        if is_prime(target, primes):
+            yield target
 
-    pi = f.read()
-    for head in range(len(pi)-digit):
-        target = int(pi[head:head+digit])
-        print(target)
-        if is_prime(target):
-            break
+if __name__ == '__main__':
+    e = open('e').read()
+    for i, v in enumerate(d_prime(e)):
+        print(i, '%010d' % v)
