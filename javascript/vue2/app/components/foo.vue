51e1f638456e6a69c35ffbe778f0c4c97c122e00
matsub <matsub.rk@gmail.com>
2017-05-11 12:56:39 +0900

    vue2

---
+++ javascript/vue2/app/components/foo.vue
@@ -0,0 +1,14 @@
+<template>
+    <p>{{ message }}</p>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      message: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
---
+++ javascript/vue2/webpack.config.js
@@ -0,0 +1,25 @@
+var path = require('path');
+
+module.exports = {
+    entry: './app/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader',
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015']
+                }
+            },
+        ]
+    }
+};
