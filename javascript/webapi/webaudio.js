ba5d9406c678adf2b97ba5c98be259f1537a195f
matsub <matsub.rk@gmail.com>
2017-08-12 15:25:48 +0900

    try: reduce channel

--- javascript/webapi/webaudio.js
+++ javascript/webapi/webaudio.js
@@ -1,19 +1,13 @@
-// create the audio context (chrome only for now)
-// create the audio context (chrome only for now)
-if (! window.AudioContext) {
-  if (! window.webkitAudioContext) {
-    alert('no audiocontext found');
-  }
-  window.AudioContext = window.webkitAudioContext;
-}
 var context = new AudioContext();
 var audioBuffer;
 var sourceNode;
 var splitter;
-var analyser, analyser2;
+var analyser;
 var javascriptNode;
+
 // get the context from the canvas to draw on
 var ctx = document.querySelector("#canvas").getContext("2d");
+
 // create a gradient for the fill. Note the strange
 // offset, since the gradient is calculated based on
 // the canvas, not the specific element we draw
@@ -22,9 +16,11 @@ gradient.addColorStop(1,'#000000');
 gradient.addColorStop(0.75,'#ff0000');
 gradient.addColorStop(0.25,'#ffff00');
 gradient.addColorStop(0,'#ffffff');
+
 // load the sound
 setupAudioNodes();
 loadSound("fly-me-to-the-moon.ogg");
+
 function setupAudioNodes() {
   // setup a javascript node
   javascriptNode = context.createScriptProcessor(2048, 1, 1);
@@ -34,9 +30,6 @@ function setupAudioNodes() {
   analyser = context.createAnalyser();
   analyser.smoothingTimeConstant = 0.3;
   analyser.fftSize = 1024;
-  analyser2 = context.createAnalyser();
-  analyser2.smoothingTimeConstant = 0.0;
-  analyser2.fftSize = 1024;
   // create a buffer source node
   sourceNode = context.createBufferSource();
   splitter = context.createChannelSplitter();
@@ -45,7 +38,6 @@ function setupAudioNodes() {
   // connect one of the outputs from the splitter to
   // the analyser
   splitter.connect(analyser,0,0);
-  splitter.connect(analyser2,1,0);
   // connect the splitter to the javascriptnode
   // we use the javascript node to draw at a
   // specific interval.
@@ -86,10 +78,6 @@ javascriptNode.onaudioprocess = function() {
   var array =  new Uint8Array(analyser.frequencyBinCount);
   analyser.getByteFrequencyData(array);
   var average = getAverageVolume(array);
-  // get the average for the second channel
-  var array2 =  new Uint8Array(analyser2.frequencyBinCount);
-  analyser2.getByteFrequencyData(array2);
-  var average2 = getAverageVolume(array2);
   // clear the current state
   ctx.clearRect(0, 0, 60, 130);
   // set the fill style
