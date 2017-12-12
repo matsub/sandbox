904035f06bfdb9f9fbc777d1c4f2f1a7ce88bfa8
matsub <matsub.rk@gmail.com>
2017-12-12 14:48:29 +0900

    add parcel

--- javascript/parcel/index.js
+++ javascript/parcel/index.js
@@ -1,2 +1,27 @@
 const vis = require("vis")
-console.log(vis)
+
+var nodes = new vis.DataSet([
+  {id: 1, label: "A"},
+  {id: 2, label: "B"},
+  {id: 3, label: "C"},
+  {id: 4, label: "D"},
+  {id: 5, label: "E"}
+])
+var edges = new vis.DataSet([
+  {from: 1, to: 3},
+  {from: 1, to: 2},
+  {from: 2, to: 4},
+  {from: 2, to: 5}
+])
+
+var container = document.getElementById('mynetwork')
+var data = { nodes, edges }
+var options = {
+  manipulation: {
+    addEdge: (edge, callback) => callback(edge)
+  }
+}
+var network = new vis.Network(container, data, options)
+
+document.getElementById('add').onclick = evt => nodes.add({id: 6, label: "new"})
+document.getElementById('connect').onclick = evt => edges.add({from: 3, to: 4})
