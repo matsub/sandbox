74c73dfeee41f662d7f5611913ea3cb33299ba12
matsub <matsub.rk@gmail.com>
2017-12-12 16:25:44 +0900

    なんだこれ

--- javascript/parcel/index.js
+++ javascript/parcel/index.js
@@ -1,17 +1,17 @@
 const vis = require("vis")
 
 var nodes = new vis.DataSet([
-  {id: 1, label: "A"},
-  {id: 2, label: "B"},
-  {id: 3, label: "C"},
-  {id: 4, label: "D"},
-  {id: 5, label: "E"}
+  {id: 'a', label: "A"},
+  {id: 'b', label: "B"},
+  {id: 'c', label: "C"},
+  {id: 'd', label: "D"},
+  {id: 'e', label: "E"}
 ])
 var edges = new vis.DataSet([
-  {from: 1, to: 3},
-  {from: 1, to: 2},
-  {from: 2, to: 4},
-  {from: 2, to: 5}
+  {from: 'a', to: 'b'},
+  {from: 'a', to: 'c'},
+  {from: 'b', to: 'd'},
+  {from: 'b', to: 'e'}
 ])
 
 var container = document.getElementById('mynetwork')
@@ -23,5 +23,5 @@ var options = {
 }
 var network = new vis.Network(container, data, options)
 
-document.getElementById('add').onclick = evt => nodes.add({id: 6, label: "new"})
-document.getElementById('connect').onclick = evt => edges.add({from: 3, to: 4})
+document.getElementById('add').onclick = evt => nodes.add({id: 'f', label: "F"})
+document.getElementById('connect').onclick = evt => edges.add({from: 'e', to: 'd'})
