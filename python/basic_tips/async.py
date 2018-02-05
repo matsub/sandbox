b3568c07e8af104d611ca618c39b4146707622a3
matsub <matsub.rk@gmail.com>
2018-02-05 19:48:50 +0900

    完全におk

--- python/basic_tips/async.py
+++ python/basic_tips/async.py
@@ -3,17 +3,25 @@
 
 import asyncio
 
-async def hello_world(loop):
+async def hello_world(loop, target='World'):
     print('Hello')
-    await asyncio.sleep(3, loop=loop)
-    print('World')
+    await asyncio.sleep(2, loop=loop)
+    print(target)
     loop.stop()
+
+
+async def sum_of(x, y):
+    await asyncio.sleep(2)
+    return x + y
 
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
-    coroutine = hello_world(loop)
-    loop.run_until_complete(coroutine)
+    coroutine = sum_of(3, 5)
+    print('running...')
+    res = loop.run_until_complete(coroutine)
+    print(res)
+
     # callback =  lambda: asyncio.ensure_future(coroutine)
     # loop.call_soon(callback)
     # loop.run_forever()
