bbbdb32eb9a27da64d94fed99a353005491fb048
matsub <matsub.rk@gmail.com>
2018-02-05 21:13:04 +0900

    もっちもち

--- python/basic_tips/async.py
+++ python/basic_tips/async.py
@@ -3,11 +3,20 @@
 
 import asyncio
 
-async def hello_world(loop, target='World'):
+async def hello_world(loop):
     print('Hello')
     await asyncio.sleep(2, loop=loop)
-    print(target)
+    print('World')
     loop.stop()
+
+
+def call_hello_world():
+    loop = asyncio.get_event_loop()
+    coroutine = hello_world(loop)
+    callback =  lambda: asyncio.ensure_future(coroutine)
+    loop.call_soon(callback)
+    loop.run_forever()
+    loop.close()
 
 
 async def sum_of(x, y):
@@ -15,14 +24,39 @@ async def sum_of(x, y):
     return x + y
 
 
-if __name__ == '__main__':
+def call_sum_of():
     loop = asyncio.get_event_loop()
     coroutine = sum_of(3, 5)
     print('running...')
     res = loop.run_until_complete(coroutine)
     print(res)
 
-    # callback =  lambda: asyncio.ensure_future(coroutine)
-    # loop.call_soon(callback)
-    # loop.run_forever()
-    # loop.close()
+
+class AsyncInstance:
+    # !!! NOTE !!!
+    # `__init__` cant be an async function 
+    # because `async def` (also `asyncio.coroutine`) returns a coroutine object.
+    # so this class makes an error.
+    # if you want to await some async function in `__init__`,
+    # you can get an event loop and run coroutine
+    # since `run_until_complete` is allowed.
+    # !!! /NOTE !!!
+    async def __init__(self, value):
+        self.value = value
+
+    def twice(self):
+        return self.value * 2
+
+
+def call_AsyncInstance():
+    loop = asyncio.get_event_loop()
+    coroutine = AsyncInstance(12)
+    print('running...')
+    ai_instance = loop.run_until_complete(coroutine)
+    print(ai_instance.twice())
+
+
+if __name__ == '__main__':
+    # call_hello_world()
+    call_sum_of()
+    # call_AsyncInstance()
