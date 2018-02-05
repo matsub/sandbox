ea6a53c01f82782c465b2c687ae0b3809d99ad75
matsub <matsub.rk@gmail.com>
2018-02-05 15:19:13 +0900

    good

---
+++ python/basic_tips/async.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import asyncio
+
+async def hello_world(loop):
+    print('Hello')
+    await asyncio.sleep(5, loop=loop)
+    print('World')
+    loop.stop()
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    coroutine = hello_world(loop)
+    callback =  lambda: asyncio.ensure_future(coroutine)
+    loop.call_soon(callback)
+    loop.run_forever()
+    loop.close()
