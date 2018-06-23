0824d592ca760f54f7351fc720ee6614f96c2f61
matsub <matsub.rk@gmail.com>
2018-06-23 20:55:59 +0900

    i gave up

--- python/lambda/ski.py
+++ python/lambda/ski.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# s = \x -> \y -> \z -> x z (y z)
+# k = \x -> \y -> x
+# i = \x -> x
+
 s = lambda x: lambda y: lambda z: x(z)( y(z) )
 k = lambda x: lambda y: x
 i = lambda x: x
 
+# church bool
 true = k
 false = k(i)
 
-k i = (lambda x: lambda y: x)(i)
-    = lambda y: i
-    = lambda y: lambda x: x
-    = lambda x: lambda y: y
+# church numeral
+zero = k(i)
