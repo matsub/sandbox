4af1b35d4ae42beed649bc82c214c1e2a0b33852
matsub <matsub.rk@gmail.com>
2018-07-05 22:25:34 +0900

    uo

--- go/basic_tips/websocket/server.go
+++ go/basic_tips/websocket/server.go
@@ -1,18 +1,27 @@
 package main
 
 import (
-	"io"
 	"net/http"
 
 	"golang.org/x/net/websocket"
 )
 
-func echo(ws *websocket.Conn) {
-	io.Copy(ws, ws)
+type Pair struct {
+	Sockets []*websocket.Conn
+}
+
+func (p Pair) Echo(ws *websocket.Conn) {
+	p.Sockets = append(p.Sockets, ws)
+
+	for _, conn := range p.Sockets {
+		conn.Write([]byte("Hello"))
+	}
 }
 
 func main() {
-	http.Handle("/echo", websocket.Handler(echo))
+	var p Pair
+
+	http.Handle("/echo", websocket.Handler(p.Echo))
 	http.Handle("/", http.FileServer(http.Dir("./statics")))
 	err := http.ListenAndServe(":8001", nil)
 	if err != nil {
