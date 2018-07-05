c33db2c60ad23313f3a1834546a48b5dadb14afd
matsub <matsub.rk@gmail.com>
2018-07-05 23:06:35 +0900

    waka tika

--- go/basic_tips/websocket/server.go
+++ go/basic_tips/websocket/server.go
@@ -2,27 +2,17 @@ package main
 
 import (
 	"net/http"
-
-	"golang.org/x/net/websocket"
 )
 
-type Pair struct {
-	Sockets []*websocket.Conn
-}
-
-func (p Pair) Echo(ws *websocket.Conn) {
-	p.Sockets = append(p.Sockets, ws)
-
-	for _, conn := range p.Sockets {
-		conn.Write([]byte("Hello"))
-	}
-}
-
 func main() {
-	var p Pair
+	hub := newHub()
+	go hub.run()
 
-	http.Handle("/echo", websocket.Handler(p.Echo))
 	http.Handle("/", http.FileServer(http.Dir("./statics")))
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		serveWs(hub, w, r)
+	})
+
 	err := http.ListenAndServe(":8001", nil)
 	if err != nil {
 		panic(err.Error())
